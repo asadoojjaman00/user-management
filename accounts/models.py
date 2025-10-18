5ec7d748118d88ed259293eff7c15825484e7480
Asadoojjaman <asadoojjaman00@gmail.com>
2025-10-18 21:42:40 +0600

    second commit

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from django.contrib.auth.models import BaseUserManager ,AbstractUser,PermissionsMixin
 # Create your models here.
+
+
+class UserManager(BaseUserManager):
+    def _create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Email is required")
+        email = self.normalize_email('email')
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using = self._db)
+        return user
+    
+    def create_superuser(self, email, password= None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self._create_user(email,password, **extra_fields)
+    
