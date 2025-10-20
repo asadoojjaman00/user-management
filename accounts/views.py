61f1ec4159021efc80fb68095a2ed1039389db90
Asadoojjaman <asadoojjaman00@gmail.com>
2025-10-20 18:42:59 +0600

    tryin to createuser with DRF

--- accounts/views.py
+++ accounts/views.py
@@ -1,2 +1,13 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import RegisterSerializer
 
+@api_view(['POST'])
+def register_view(request):
+    serializer = RegisterSerializer(data = request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response({"message": "user created successfully"}, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
